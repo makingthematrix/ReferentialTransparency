2a1bf2aa537670e8e8f5ba1dafd0cc50e335e573
makingthematrix <makingthematrix@protonmail.com>
2025-10-23 13:14:37 +0200

    MUnitVersion

--- build.sbt
+++ build.sbt
@@ -1,10 +1,48 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.3.3"
+val _scalaVersion = "3.7.2"
 
-libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.4"
+ThisBuild / version := "0.1.0"
+
+organization := "io.github.makingthematrix"
+name := "ReferentialTransparency"
+licenses := Seq("GPL 3.0" -> url("https://www.gnu.org/licenses/gpl-3.0.en.html"))
+ThisBuild / scalaVersion := _scalaVersion
+ThisBuild / versionScheme := Some("semver-spec")
+Test / scalaVersion := _scalaVersion
+
+developers := List(
+  Developer(
+    "makingthematrix",
+    "Maciej Gorywoda",
+    "makingthematrix@protonmail.com",
+    url("https://github.com/makingthematrix"))
+)
+
+val standardOptions = Seq(
+  "-deprecation",
+  "-feature",
+  "-unchecked",
+  "-encoding",
+  "utf8"
+)
+
+val scala3Options = Seq(
+  "-explain",
+  "-Wsafe-init",
+  "-Ycheck-all-patmat",
+  "-Wunused:imports",
+  "-no-indent", "-rewrite"
+)
 
 lazy val root = (project in file("."))
   .settings(
-    name := "ReferentialTransparency"
+    name := "ReferentialTransparency",
+    libraryDependencies ++= Seq(
+      "org.typelevel" %% "cats-effect" % "3.6.3",
+      //Test dependencies
+      "org.scalameta" %% "munit" % "1.2.0" % "test"
+    ),
+    scalacOptions ++= standardOptions ++ scala3Options
   )
+
+testFrameworks += new TestFramework("munit.Framework")
--- src/main/scala/MUnitVersion.scala
+++ src/main/scala/MUnitVersion.scala
@@ -1,33 +1,42 @@
 import java.nio.file.{Files, Path, Paths}
 import scala.jdk.CollectionConverters.*
 
-object SideEffectsVersion {
-  def main(): Unit = {
-    val lines        = read(FilePath)
+class MUnitVersion(read        : () => List[String], 
+                   askForUpdate: () => Int, 
+                   write       : List[String] => Unit) {
+  def run(): Unit = {
+    val lines        = read()
     val protagonists = lines.map(Protagonist.fromLine)
     val n            = askForUpdate()
-    val updated      = protagonists.map(updateAge(_, n))
+    val updated      = protagonists.map(MUnitVersion.updateAge(_, n))
     val newLines     = updated.map(_.toLine)
-    write(FilePath, newLines)
+    write(newLines)
   }
+}
+
+object MUnitVersion {
+  @main def main(): Unit = MUnitVersion().run()
   
+  def apply(): MUnitVersion =
+    new MUnitVersion(
+      read         = () => read(FilePath),
+      askForUpdate = askForUpdate,
+      write        = write(FilePath, _)
+    )
+
   private val FilePath: Path = Paths.get("resources/protagonists.csv")
+
+  private def updateAge(p: Protagonist, n: Int): Protagonist = p.copy(age = p.age + n)
 
   private def read(path: Path): List[String] =
     Files.readAllLines(path).asScala.toList
-  
+
   private def askForUpdate(): Int = {
     printf("By how much should I update the age? ")
     val answer = scala.io.StdIn.readLine()
     answer.toInt
   }
-  
-  private def updateAge(p: Protagonist, n: Int): Protagonist = {
-    val newAge = p.age + n
-    println(s"The age of ${p.firstName} ${p.lastName} changes from ${p.age} to $newAge")
-    p.copy(age = newAge)
-  }
-  
+
   private def write(path: Path, lines: List[String]): Unit =
     Files.writeString(path, lines.mkString("\n"))
 }
